c4c1e5b51c22097525cf8b4e20bcd9a23a98bc5d
Zachary Parr <zacharyparr@rocketmortgage.com>
2021-12-07 19:16:48 -0500

    first commit

---
+++ BlockbusterDapper/Controllers/MovieController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlockbusterDapper.Models;
+using Dapper;
+
+namespace BlockbusterDapper.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            Movies m = new Movies();
+            return View(m);
+        }
+        
+        [HttpPost]
+        public IActionResult Index(Movies m)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var connect = new MySqlConnection(Secret.Connection))
+                {
+
+                    string insertString = $"insert into movie values(0,@name,@genre,@year,@runtime)";
+                    string queryString = $"select * from movie";
+
+                    connect.Open();
+                    connect.Execute(insertString, new
+                    {
+                        Title = m.Name,
+                        Genre = m.Genre,
+                        Year = m.Year,
+                        Runtime = m.Runtime
+                    });
+                    List<Movies> inventory = connect.Query<Movies>(queryString).ToList();
+                    connect.Close();
+
+                    return RedirectToAction("List", "Movie");
+                }
+            }
+            else
+            {
+                return View(m);
+            }
+        }
+
+        public IActionResult List()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+
+
+                string queryString = $"select * from movies";
+
+                connect.Open();
+                List<Movies> inventory = connect.Query<Movies>(queryString).ToList();
+                connect.Close();
+                return View(inventory);
+            }
+
+        }
+
+    }
+}
---
+++ BlockbusterDapper/Models/Movies.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockbusterDapper.Models
+{
+    public class Movies
+    {
+        /*
+        id int not null auto_increment primary key, 
+        `name` nvarchar(50) not null, 
+         `genre` nvarchar(20) not null,
+         `year` int, 
+         runtime time
+         */
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "Name of movie must be at least 50 characters")] //i didn't know how to fix the SQL requirements to make this 30 so just ran with 50
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        [Range(1880, 2021, ErrorMessage = "Movie must be between the years 1880 and 2021.")]
+        public int Year { get; set; }
+        public DateTime Runtime { get; set; }
+    }
+}
